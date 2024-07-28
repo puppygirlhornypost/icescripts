1efffde6769cac9d8a4e7e8a97c8c618606b38bd
puppygirlhornypost <puppygirlhornyposting@gmail.com>
2024-07-28 17:30:00 -0500

    refactored to use die, changed end to guard

--- admin/gentoken.pl
+++ admin/gentoken.pl
@@ -42,8 +42,7 @@ my $res = $ua->request($req);
 # Handle the result
 if ($res->code != 200) {
   print("shit is fucked.\n");
-  print($res->as_string);
-  exit 1;
+  die $res->as_string;
 }
 
 my $token = decode_json($res->decoded_content)->{token};
@@ -57,8 +56,7 @@ my $authreq = $ua->request(HTTP::Request->new('GET', $instance.$auth));
 # Guard statement for unexpected http code
 if ($authreq->code != 200) {
   print("shit is fucked.\n");
-  print($authreq->as_string);
-  exit 1;
+  die $authreq->as_string;
 }
 
 print("Success! Token: `$token`\n");
