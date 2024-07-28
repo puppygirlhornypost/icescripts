020bdce7c574922a1d821e25943390f385ec3fb6
puppygirlhornypost <puppygirlhornyposting@gmail.com>
2024-07-28 14:04:17 -0500

    added generate_invite.pl to git

---
+++ admin/generate_invite.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use HTTP::Request();
+use JSON::MaybeXS qw(encode_json decode_json);
+use LWP::UserAgent();
+
+my $bearer = '';
+my $instance = 'https://ice.puppygirl.sale';
+my $endpoint = '/api/iceshrimp/admin/invites/generate';
+
+my $data = {};
+my $encoded_data = encode_json($data);
+
+# Create the request with appropriate headers
+my $req = HTTP::Request->new('POST', $instance.$endpoint);
+$req->content($encoded_data);
+my $ua = LWP::UserAgent->new();
+$ua->default_header('Content-Type' => 'application/json');
+$ua->default_header('Host' => 'ice.puppygirl.sale');
+$ua->default_header('Authorization' => "Bearer $bearer");
+$ua->agent('amber inv script');
+
+# Send the request
+my $res = $ua->request($req);
+
+# Handle the result
+if ($res->code == 200) {
+  my $code = decode_json($res->decoded_content)->{code};
+  print("Success! Code: `$code`\n");
+} else {
+  print("shit is fucked.\n");
+  print($res->as_string);
+}
+
