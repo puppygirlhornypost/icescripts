fe398d9ead97eafcc75d625bacb06c358486d5c3
puppygirlhornypost <puppygirlhornyposting@gmail.com>
2024-08-05 21:23:55 -0500

    Update gentoken.pl
    Utilizes getopt

--- admin/gentoken.pl
+++ admin/gentoken.pl
@@ -6,34 +6,70 @@ use warnings;
 use HTTP::Request();
 use JSON::MaybeXS qw(encode_json decode_json);
 use LWP::UserAgent();
+use Getopt::Long;
 
-# USER MODIFIABLE SETTINGS
-my $username = '';
-my $password = '';
-# Instance URL is just something like "https://ice.puppygirl.sale"
-my $instance = '';
-# instance url without https:// (according to docs this is used for register
-# endpoint.) "ice.puppygirl.sale"
-my $host = '';
+# If you want to hard code values instead of using cli, change undef to 'value'.
+# 
+# example:
+#   username => 'username'
+# 
+# Instance is the instance url, and host is the host url. 
+# 
+# example:
+#   instance => 'https://ice.puppygirl.sale/'
+#   host => 'ice.puppygirl.sale'
+#
+# If you want to skip verification, set skip_verify => 1
+
+my %config = (
+  username => undef,
+  password => undef,
+  instance => undef,
+  host => undef,
+  skip_verify => ''
+);
+
 # END USER MODIFIABLE SETTINGS
 
+GetOptions(
+  'username=s' => \$config{username},
+  'password=s' => \$config{password},
+  'instance=s' => \$config{instance},
+  'host=s' => \$config{host},
+  'skip-verify' => \$config{skip_verify}
+);
+
+if (!$config{username}) {
+  die "username is undefined";
+}
+
+if (!$config{password}) {
+  die "password is undefined";
+}
+
+if (!$config{instance}) {
+  die "instance is undefined";
+}
+
+if (!$config{host}) {
+  die "host is undefined";
+}
+
+# api endpoints
 my $auth = '/api/iceshrimp/auth';
 my $login = $auth.'/login';
 
-# yeah i hate it too dw
-my $data = {
-  username => "$username",
-  password => "$password"
-};
-
-my $encoded_data = encode_json($data);
-
 # Create the request with appropriate headers
-my $req = HTTP::Request->new('POST', $instance.$login);
-$req->content($encoded_data);
+my $req = HTTP::Request->new('POST', $config{instance}.$login);
+
+$req->content(encode_json({
+  username => $config{username},
+  password => $config{password}
+}));
+
 my $ua = LWP::UserAgent->new();
 $ua->default_header('Content-Type' => 'application/json');
-$ua->default_header('Host' => 'ice.puppygirl.sale');
+$ua->default_header('Host' => $config{host});
 $ua->agent('amber token script');
 
 # Send the request
@@ -46,12 +82,18 @@ if ($res->code != 200) {
 }
 
 my $token = decode_json($res->decoded_content)->{token};
+
+if ('' ne $config{skip_verify}) {
+  print("Success! Token: `$token`\n");
+  exit;
+}
+
 print("Server responded with `$token`, verifying...\n");
 
 $ua->default_headers->remove_header('Content-Type');
 $ua->default_header('Accept' => 'application/json');
 $ua->default_header('Authorization' => "Bearer $token");
-my $authreq = $ua->request(HTTP::Request->new('GET', $instance.$auth));
+my $authreq = $ua->request(HTTP::Request->new('GET', $config{instance}.$auth));
 
 # Guard statement for unexpected http code
 if ($authreq->code != 200) {
